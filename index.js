bfed017c922ece22a9d008278d7c8b0fcc1f563e
Jillianmwangi <mwangyjill@gmail.com>
2023-02-06 04:19:23 +0300

    add new todoappp

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 let body = document.body;
 const tasktitle = document.querySelector(".tasktitle");
-const description =document.querySelector("description");
-const completiondate = document.querySelector("completiondate");
+const description =document.querySelector(".description");
+const completiondate = document.querySelector(".completiondate");
 const taskbtn = document.querySelector(".submit");    
-const tasks = document.querySelector("tasklist")
-const dropdown = document.querySelector("dropdown");
+const tasks = document.querySelector(".tasklist");
+const dropdown = document.querySelector(".dropdown");
+const complete_tasks= document.querySelector(".complete_tasks");
+const complist= document.querySelector(".complist");
+let taskarr = [];
 
-taskbtn.addEventListener('click');
-tasks.addEventListener('click');
-dropdown.addEventListener('change');
-
-
-function addtask(event) {
-event.preventdefault();
-
-    let div = document.createElement("div");   
-    div.classList.add('tasksdiv');
-    body.appendChild(div);
-
-    let taskdisp = document.createElement("li");
-    taskdisp.classList.add('taskdisp');
-    taskdisp.innerText = tasktitle.value;
-    div.appendChild(taskdisp);
-      
-    tasklist.appendChild(div);   
-
-    function savetolocalstorage(tasks){
-      localStorage.setItem("tasks",task)
-    };
-    const completebutton = document.createElement("button");
-    completebutton.innerHTML =<i class="fa-solid fa-check"></i>;
-    
-    const trashbutton = document.createElement("button");
-    trashbutton.innerHTML = <i class="fa-solid fa-trash"></i>;
+let task = {
+    title:"", 
+    description:"",
+    time:"",
+    complete:false 
 };
+
+ let title = tasktitle.addEventListener('input', ()=>{
+    task.title = tasktitle.value});
+
+ let desc = description.addEventListener('input', ()=>{
+    task.description = description.value});
+
+let time = completiondate.addEventListener('input', ()=>{
+    task.time = completiondate.value});
+
+taskarr.push(task);
+
+function displaytasks(){
+
+ taskarr.forEach(job=>{
+     tasks.innerHTML += 
+     `<li>
+       <p>Todo: ${job.title} </p>
+       <p>Description:${job.description}</p>
+       <p>Completion date:${job.time}</p>
+       <input type= "checkbox" >
+       <button onclick=" delete " class="delete"> Delete</button>
+      </li>`
+    
+      function sliceArray(taskarr, checkbox) {
+        if (checkbox.checked) {
+          return taskarr.slice(1, 1);
+        } else {
+          return taskarr;
+        }
+      }
+      
+    
+    });
+
+ console.log(taskarr);
+}
+
+taskbtn.addEventListener('click', displaytasks);
+
 
 
